db5c1beaf138457a880df9b782b7ae01b0103329
Pindar <spd10@163.com>
2018-10-10 07:24:43 +0800

    1

--- src/router/BasicExample.js
+++ src/router/BasicExample.js
@@ -1,74 +1,76 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Prompt } from "react-router-dom";
 
 const BasicExample = () => {
-    const getConfirmation = (message, callback) => {
-        const confirm = window.confirm(message);
-        callback(confirm);
-    };
-    return <Router forceRefresh={false} getUserConfirmation={getConfirmation}>
-        <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/topics">Topics</Link>
-                </li>
-            </ul>
+  const getConfirmation = (message, callback) => {
+    const allowTransition = window.confirm(message);
+    callback(allowTransition);
+  };
+  return (
+    <Router forceRefresh={false} getUserConfirmation={getConfirmation}>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+        <Prompt message="are you sure want to leave me."/>
+        <hr />
 
-            <hr/>
-
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About}/>
-            <Route path="/topics" component={Topics}/>
-        </div>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
     </Router>
+  );
 };
 
 const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
+  <div>
+    <h2>Home</h2>
+  </div>
 );
 
 const About = () => (
-    <div>
-        <h2>About</h2>
-    </div>
+  <div>
+    <h2>About</h2>
+  </div>
 );
 
 const Topics = ({ match }) => (
-    <div>
-        <h2>Topics</h2>
-        <ul>
-            <li>
-                <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/components`}>Components</Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-            </li>
-        </ul>
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
 
-        <Route path={`${match.url}/:topicId`} component={Topic} />
-        <Route
-            exact
-            path={match.url}
-            render={() => <h3>Please select a topic.</h3>}
-        />
-    </div>
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
 );
 
 const Topic = ({ match }) => (
-    <div>
-        <h3>{match.params.topicId}</h3>
-    </div>
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
 );
 
 export default BasicExample;
